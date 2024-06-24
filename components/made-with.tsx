2a6409db8ece47af5b6f829a6fe92090e6be066c
Sullivan <38718448+Epic-R-R@users.noreply.github.com>
2024-06-24 18:56:25 +0330

    Update

--- components/made-with.tsx
+++ components/made-with.tsx
@@ -1,18 +1,20 @@
 import {Link} from "@nextui-org/link";
 import {HeartFilledIcon} from "@/components/icons";
+import {siteConfig} from "@/config/site";
 
 export const MadeWith = () => {
     return (
         <div className="w-full flex items-center justify-center py-3">
             <Link
                 isExternal
+                color="foreground"
                 className="flex items-center gap-1 text-current"
-                href="https://gavazn.life"
-                title="Homepage"
+                href={siteConfig.links.twitter}
+                title="Twitter Sullivan"
             >
-                <span className="text-default-600">Made with </span>
+                <span>Made with </span>
                 <HeartFilledIcon color="red"/>
-                <span className="text-default-600"> By </span>
+                <span> By </span>
                 <p className="text-foreground">Sullivan</p>
             </Link>
         </div>
